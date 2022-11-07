5be3aa6d97d582d49b2e8499fb7796646cd5b324
Borja <borjadg38@gmail.com>
2022-11-07 12:10:46 +0100

    Update HelloArActivityTest2.java

--- app/src/androidTest/java/com/google/ar/core/examples/java/helloar/HelloArActivityTest2.java
+++ app/src/androidTest/java/com/google/ar/core/examples/java/helloar/HelloArActivityTest2.java
@@ -73,193 +73,12 @@ public class HelloArActivityTest2 {
 
     @Test
     public void helloArActivityTest() throws InterruptedException {
-        //wait(2000);
-        //NEWCODE: Next Removed
-        /*ViewInteraction appCompatButton = onView(
-                allOf(withId(R.id.playback_button), withText("Playback"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(android.R.id.content),
-                                        0),
-                                3),
-                        isDisplayed()));
-        appCompatButton.perform(click());
-        Thread.sleep(2000);*/
 
-        // Click on the recorded video (in Downloads file)
-        // Long tap (select) on a video in the gallery
-        //First video OK
+        //FOR A CERTAIN 3D OBJECT
+        double xLength = 0, yLength = 0, zLength = 0;
 
-        //NEWCODE: Next Removed
-        //UiDevice.getInstance(InstrumentationRegistry.getInstrumentation()).click(200, 1000);
-
-        //Second video OK
-//        UiDevice.getInstance(InstrumentationRegistry.getInstrumentation()).click(800, 1000);
-        //Third video OK
-//        UiDevice.getInstance(InstrumentationRegistry.getInstrumentation()).click(200, 1500);
-        //Forth video OK
-//        UiDevice.getInstance(InstrumentationRegistry.getInstrumentation()).click(800, 1500);
-
-        // Tap on SELECT
-        /*Thread.sleep(1000);
-        UiDevice.getInstance(InstrumentationRegistry.getInstrumentation()).click(900, 300);*/
-
-        //NEWCODE: Next Removed
-        //Thread.sleep(40000);
-
-        //Tap the screen to place an item
-        //onView(withId(R.id.surfaceview)).perform(touchDownAndUp(50, 50));
-
-        //NEWCODE: Next Removed
-        /*UiDevice.getInstance(InstrumentationRegistry.getInstrumentation()).click(300, 1500);
-        Thread.sleep(3000);
-        UiDevice.getInstance(InstrumentationRegistry.getInstrumentation()).click(600, 1500);
-        Thread.sleep(3000);
-        UiDevice.getInstance(InstrumentationRegistry.getInstrumentation()).click(400, 1000);
-        Thread.sleep(3000);
-        UiDevice.getInstance(InstrumentationRegistry.getInstrumentation()).click(500, 1000);
-        Thread.sleep(5000);*/
-
-        //Validate properties of mActivityTestRule
-
-        /*ActivityScenario scenario = mActivityTestRule.getScenario();
-        mActivityTestRule.getScenario().getResult().getResultData().get
-        scenario.getResult().getResultData();
-        Activity activityInstance = getActivityInstance();*/
-        /*assertThat(activity.getSomething()).isEqualTo("something");
-        assertEquals(datosRecibidos.extras!!.getString("nombre"), "Julio")
-        scenario.onActivity(activity -> {
-            assertThat(activity.getSomething()).isEqualTo("something");
-            assertThat(activity.activityResult.getResultData()).extras().containsKey("key");
-        });*/
-
-        String TAG = "MyActivity";
-        List<WrappedAnchor> anchorsList = mActivityTestRule.getActivity().getAnchors();
-        Log.d(TAG, "Number of anchors: " + anchorsList.size());
-
-        //NEWCODE: Next Removed
-        /*for(WrappedAnchor wAnchor : anchorsList) {
-            Anchor anchor = wAnchor.getAnchor();
-            float qx = anchor.getPose().qx();
-            float qy = anchor.getPose().qy();
-            float qz = anchor.getPose().qz();
-            float qw = anchor.getPose().qw();
-
-            //Item coordinates
-            float tx = anchor.getPose().tx();
-            float ty = anchor.getPose().ty();
-            float tz = anchor.getPose().tz();
-
-            float[] xAxis = anchor.getPose().getXAxis();
-            float[] yAxis = anchor.getPose().getYAxis();
-            float[] zAxis = anchor.getPose().getZAxis();
-
-            Log.d(TAG, "qx = " + qx);
-            Log.d(TAG, "qy = " + qy);
-            Log.d(TAG, "qz = " + qz);
-            Log.d(TAG, "qw = " + qw);
-            Log.d(TAG, "tx = " + tx);
-            Log.d(TAG, "ty = " + ty);
-            Log.d(TAG, "tz = " + tz);
-
-            Log.d(TAG, "xAxis:");
-            for(float i : xAxis){
-                Log.d(TAG, "" + i);
-            }
-            Log.d(TAG, "yAxis:");
-            for(float i : yAxis){
-                Log.d(TAG, "" + i);
-            }
-            Log.d(TAG, "zAxis:");
-            for(float i : zAxis){
-                Log.d(TAG, "" + i);
-            }*/
-        System.out.println(" *** NEW CODE BEGINS ***");
-
-            //Item hitbox
-        /*
-            try {
-                //Try to print test from the .obj
-                //File myObj = new File("pawn.obj");
-                //File myObj = new File("app/src/main/assets/models/pawn.obj");
-                //File myObj = new File("app/src/androidTest/java/com/google/ar/core/examples/java/helloar/pawn.obj");
-                //File myObj = new File("app/src/androidTest/java/com/google/ar/core/examples/java/helloar/pawn.obj");
-
-                //Use pawn.txt, that contains the text from pawn.obj
-                //File myObj = new File("app/src/main/assets/models/pawn.txt");
-                //File myObj = new File("pawn.txt");
-                //File myObj = new File("app/assets/models/pawn.txt");
-                //File myObj = new File("app/assets/models/prov.txt");
-                //File myObj = new File("app/src/main/assets/models/prov.txt");
-                //File myObj = new File("prov.txt");
-                //File myObj = new File(new File("app/src/main/assets/models/prov.txt").getAbsolutePath());
-                //File myObj = new File(path.trim());
-                //File myObj = new File(Objects.requireNonNull(HelloArActivityTest2.class.getResource("app/src/main/assets/models/prov.txt")).toURI());
-
-                //System.out.println("user.dir = " + System.getProperty("user.dir"));
-                //String path = System.getProperty("user.dir") + "src/androidTest/prov.txt";
-                //String path = System.getProperty("user.dir") + "\\src\\androidTest\\prov.txt";
-                //MockHttpServletRequest request = new MockHttpServletRequest();
-                //String path = request.getSession().getServletContext().getRealPath("/");
-
-                File myObj = new File("app/src/main/assets/models/prov.txt");
-
-                //myObj.setReadable(true);
-                //myObj.setWritable(true);
-
-                System.out.println("myObj.mkdirs() = " + myObj.mkdirs());   //false
-                System.out.println("myObj.exists() = " + myObj.exists());   //false
-                System.out.println("myObj.isDirectory() = " + myObj.isDirectory()); //false
-                System.out.println("myObj.canRead() = " + myObj.canRead()); //false
-                System.out.println("Current directory: " + new File(".").getAbsolutePath());    // /.
-
-                Scanner myReader = new Scanner(myObj);
-                int counter = 0;
-                while (myReader.hasNextLine() && counter <= 10) {
-                    String data = myReader.nextLine();
-                    System.out.println(data);
-                    counter ++;
-                }
-                myReader.close();
-
-
-
-                //Check if a line begins with "v ", and store the coordinates
-
-
-            } catch (FileNotFoundException e) {
-                System.out.println("An error occurred.");
-                e.printStackTrace();
-            }
-            */
-
-
-        /*
-        try (BufferedReader in = new BufferedReader(new InputStreamReader(context.getAssets().open("prov.txt")))) {
-            System.out.println("*** TRY CP1");
-            in.lines().limit(10).forEach(System.out::println);
-            System.out.println("*** TRY CP2");
-        }
-        catch(IOException e) {
-            e.printStackTrace();
-        }
-         */
-
-/*
-        String string = "";
-        try {
-            InputStream inputStream = getAssets().open("myText.txt");
-            int size = inputStream.available();
-            byte[] buffer = new byte[size];
-            inputStream.read(buffer);
-            string = new String(buffer);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        System.out.println("Printed String: " + string);
- */
-
-
+        // Hitbox of 3D object
+        // Max and min values of X, Y, Z coordinates
         try {
             boolean vertexReached = false;
             double[] maxCoordinates = {0, 0, 0};
@@ -303,23 +122,113 @@ public class HelloArActivityTest2 {
             System.out.println("^^^^ maxCoordinates: [" + maxCoordinates[0] + ", " + maxCoordinates[1] + ", " + maxCoordinates[2] + "]");
             System.out.println("^^^^ minCoordinates: [" + minCoordinates[0] + ", " + minCoordinates[1] + ", " + minCoordinates[2] + "]");
 
+            xLength = maxCoordinates[0] - minCoordinates[0];
+            yLength = maxCoordinates[0] - minCoordinates[0];
+            zLength = maxCoordinates[0] - minCoordinates[0];
+
             in.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
 
+        // Select video, tap screen, etc
+        //wait(2000);
+        ViewInteraction appCompatButton = onView(
+                allOf(withId(R.id.playback_button), withText("Playback"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                3),
+                        isDisplayed()));
+        appCompatButton.perform(click());
+        Thread.sleep(2000);
 
+        // Click on the recorded video (in Downloads file)
+        // Long tap (select) on a video in the gallery
+        //First video OK
 
-            System.out.println(" *** NEW CODE ENDS ***");
+        UiDevice.getInstance(InstrumentationRegistry.getInstrumentation()).click(200, 1000);
 
+        //Second video OK
+//        UiDevice.getInstance(InstrumentationRegistry.getInstrumentation()).click(800, 1000);
+        //Third video OK
+//        UiDevice.getInstance(InstrumentationRegistry.getInstrumentation()).click(200, 1500);
+        //Forth video OK
+//        UiDevice.getInstance(InstrumentationRegistry.getInstrumentation()).click(800, 1500);
 
+        // Tap on SELECT
+        /*Thread.sleep(1000);
+        UiDevice.getInstance(InstrumentationRegistry.getInstrumentation()).click(900, 300);*/
+
+        Thread.sleep(40000);
+
+        //Tap the screen to place an item
+        //onView(withId(R.id.surfaceview)).perform(touchDownAndUp(50, 50));
+
+        UiDevice.getInstance(InstrumentationRegistry.getInstrumentation()).click(300, 1500);
+        Thread.sleep(3000);
+
+        //NEWCODE: DELETE
+        /*UiDevice.getInstance(InstrumentationRegistry.getInstrumentation()).click(600, 1500);
+        Thread.sleep(3000);
+        UiDevice.getInstance(InstrumentationRegistry.getInstrumentation()).click(400, 1000);
+        Thread.sleep(3000);
+        UiDevice.getInstance(InstrumentationRegistry.getInstrumentation()).click(500, 1000);
+        Thread.sleep(5000);
+         */
+
+        // Check position of 3D objects (translation)
+        String TAG = "MyActivity";
+        List<WrappedAnchor> anchorsList = mActivityTestRule.getActivity().getAnchors();
+        Log.d(TAG, "Number of anchors: " + anchorsList.size());
+
+        for(WrappedAnchor wAnchor : anchorsList) {
+            Anchor anchor = wAnchor.getAnchor();
+
+            //Item coordinates
+            float tx = anchor.getPose().tx();
+            float ty = anchor.getPose().ty();
+            float tz = anchor.getPose().tz();
+
+            System.out.println("^^^^ Translation X: " + tx);
+            System.out.println("^^^^ Translation Y: " + ty);
+            System.out.println("^^^^ Translation Z: " + tz);
+
+            //Calculate max and min positions in real space for each coordinate (hitbox in real space)
+            double hitboxMinX = tx - (xLength/2);
+            double hitboxMaxX = tx + (xLength/2);
+            double hitboxMinY = ty - (yLength/2);
+            double hitboxMaxY = ty + (yLength/2);
+            double hitboxMinZ = tz - (zLength/2);
+            double hitboxMaxZ = tz + (zLength/2);
+
+            // Upload the hitbox list
+            List<double[]> hitboxList = new ArrayList<double[]>();
+            double[] hitbox = new double[6];
+            hitbox[0] = hitboxMinX;
+            hitbox[1] = hitboxMaxX;
+            hitbox[2] = hitboxMinY;
+            hitbox[3] = hitboxMaxY;
+            hitbox[4] = hitboxMinZ;
+            hitbox[5] = hitboxMaxZ;
+            hitboxList.add(hitbox);
+
+            System.out.println("^^^^ Hitbox of the 3D object:");
+            System.out.println("^^^^ hitboxMinX: " + hitbox[0]);
+            System.out.println("^^^^ hitboxMaxX: " + hitbox[1]);
+            System.out.println("^^^^ hitboxMinY: " + hitbox[2]);
+            System.out.println("^^^^ hitboxMaxY: " + hitbox[3]);
+            System.out.println("^^^^ hitboxMinZ: " + hitbox[4]);
+            System.out.println("^^^^ hitboxMaxZ: " + hitbox[5]);
+
+            //TODO: Check if there are intersections of hitboxes
             /*assertTrue(qx == 0.0);
             assertTrue(qz == 0.0);*/
         }
 
-        //NEWCODE: Next Removed
-        /*Thread.sleep(10000);
-    }*/
+        Thread.sleep(10000);
+    }
 
     public static Matcher<View> childAtPosition(
             final Matcher<View> parentMatcher, final int position) {
